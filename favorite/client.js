958ddc12a58b47476a22b2f4f6025f02321797e2
opellacani <no4meis@gmail.com>
2024-10-12 10:58:50 -0300

    0.1.8-test7

--- favorite/client.js
+++ favorite/client.js
@@ -1,1 +1,1 @@
-javascript:(function (){ var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/cmsp-peladinho@0.1.8-test6/index.js"; document.body.append(script) } )();
+javascript:(function (){ var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/cmsp-peladinho@0.1.8-test7/index.js"; document.body.append(script) } )();
